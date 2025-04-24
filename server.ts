b9d8e568af0e77ff99eca19b13c7344a7af1485b
Favas K P <183742743+favas-kp@users.noreply.github.com>
2025-04-24 14:59:45 +0530

    [Favas] | Fixed bug
    Player manager was not having id generator

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
 import { createApp } from "./src/app.ts";
 import { GameManager } from "./src/models/gameManager.ts";
 import { LobbyManager } from "./src/models/lobby.ts";
-import { PlayerManager } from "./src/models/playerManager.ts";
+import { getIdGenerator, PlayerManager } from "./src/models/playerManager.ts";
 
 const main = () => {
-  const playerManager = new PlayerManager();
+  const playerManager = new PlayerManager(getIdGenerator());
   const lobbyManager = new LobbyManager();
   const gameManager = new GameManager();
 
