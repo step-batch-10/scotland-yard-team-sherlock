9dff264b0c153df86c7bf5728701838e4fa11f1f
Deepanshu Tiwari <183710540+deepanshutiwari16@users.noreply.github.com>
2025-04-24 10:59:33 +0530

    [#0] | [Beauti, Deepanshu, Asma] | Improves routing structures

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,31 @@ import { PlayerManager } from "./models/playerManager.ts";
 import { LobbyManager } from "./models/lobby.ts";
 import { GameManager } from "./models/gameManager.ts";
 
+const createLobbyRoutes = () => {
+  const app = new Hono();
+
+  app.use(validatePlayerId);
+  app.post("/quick-play", checkRoomRejoin, handleGameJoin);
+  app.post("/room/join", validateJoin, joinUser);
+  app.use("/room/status", checkGameStart);
+  app.use(validateRoomId);
+  app.get("/room/status", serveRoomStatus);
+  app.post("/room/leave", leaveLobby);
+
+  return app;
+};
+
+const createGameRoutes = () => {
+  const app = new Hono();
+
+  app.use(validatePlayerId, validateGameId);
+  app.get("/details", assignRoles);
+  app.post("/move", makeMove);
+  app.get("/status", serveGameStatus);
+
+  return app;
+};
+
 export const createApp = (
   playerManager: PlayerManager,
   lobbyManager: LobbyManager,
@@ -36,6 +61,9 @@ export const createApp = (
 ) => {
   const app = new Hono();
   app.use(logger());
+
+  const lobbyRoutes = createLobbyRoutes();
+  const gameRoutes = createGameRoutes();
 
   app.use(async (context: Context, next: Next) => {
     context.set("playerManager", playerManager);
@@ -47,36 +75,21 @@ export const createApp = (
   app.get("/", validatePlayerId, serveIndex);
   app.get("/index.html", validatePlayerId, serveIndex);
   app.get("/login.html", handleLoginAccess);
-  app.get(
-    "/waiting.html",
-    validatePlayerId,
-    handleWaitingAccess,
-    serveWaitingPage,
-  );
-  app.get("/game.html", validatePlayerId, validateGameId);
+  app
+    .get(
+      "/waiting.html",
+      validatePlayerId,
+      handleWaitingAccess,
+      serveWaitingPage,
+    );
+
+  app.use("/game.html", validatePlayerId, validateGameId);
 
   app.post("/auth/login", login);
   app.get("/auth/logout", logout);
 
-  app.post(
-    "/lobby/quick-play",
-    validatePlayerId,
-    checkRoomRejoin,
-    handleGameJoin,
-  );
-  app.post("/lobby/room/join", validatePlayerId, validateJoin, joinUser);
-  app.get(
-    "/lobby/room/status",
-    validatePlayerId,
-    checkGameStart,
-    validateRoomId,
-    serveRoomStatus,
-  );
-  app.post("/lobby/room/leave", validatePlayerId, validateRoomId, leaveLobby);
-
-  app.get("/game/details", validatePlayerId, validateGameId, assignRoles);
-  app.get("/game/status", validatePlayerId, validateGameId, serveGameStatus);
-  app.post("/game/move", validatePlayerId, validateGameId, makeMove);
+  app.route("/lobby", lobbyRoutes);
+  app.route("/game", gameRoutes);
 
   app.use("*", serveStatic({ root: "./public" }));
 
